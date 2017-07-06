798d88e60da579d4cf5632d5efc128c94695ce7b
Don Cooper <don_cooper@hotmail.com>
2017-07-06 14:16:05 -0400

    Switch turning off, background turning dark, status changing when light switch is clicked

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  var status = document.querySelector('.status')
-  status.innerText = 'Document is ready.'
+
+  var lightSwitch = document.querySelector('button');
+  lightSwitch.addEventListener('click', function(event) {
+    lightSwitch.className = 'switch off';
+    var body = document.querySelector('body');
+    body.className = 'dark';
+    var status = document.querySelector('.status');
+    status.innerText = 'Hey, who turned off the lights?';
+  });
+
+
 });
