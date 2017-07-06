faf19d359f37d00f5732099d10dae948a42ef074
Don Cooper <don_cooper@hotmail.com>
2017-07-06 14:22:26 -0400

    Exercise complete

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
   var lightSwitch = document.querySelector('button');
+  var body = document.querySelector('body');
+  var status = document.querySelector('.status');
+
   lightSwitch.addEventListener('click', function(event) {
-    lightSwitch.className = 'switch off';
-    var body = document.querySelector('body');
-    body.className = 'dark';
-    var status = document.querySelector('.status');
-    status.innerText = 'Hey, who turned off the lights?';
+
+    if (lightSwitch.className === 'switch on') {
+      lightSwitch.className = 'switch off';
+      body.className = 'dark';
+      status.innerText = 'Hey, who turned off the lights?';
+    } else {
+      lightSwitch.className = 'switch on';
+      body.className = 'light';
+      status.innerText = "It's so bright in here!";
+    }
   });
-
-
 });
